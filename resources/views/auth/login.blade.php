16664d08efc378857f32752d5df8e4ba5da85a6e
himanshusaini9024 <sainihimanshu208@gmail.com>
2026-05-30 10:27:36 +0530

    changes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,21 +14,21 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-12 col-md-9 mt-5">
+      <div class="col-xl-6 col-lg-12 col-md-9 mt-5">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              <div class="col-lg-12">
                 <div class="p-5">
-                  <div class="text-center">
+                  <div class="text-center ">
                     <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                    <strong class="h4 text-gray-900 mb-4 ">DHIRAGO PANEL</strong>
                   </div>
                   <form class="user"  method="POST" action="{{ route('login') }}">
                     @csrf
-                    <div class="form-group">
+                    <div class="form-group mt-5">
                       <input type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address..."  required autocomplete="email" autofocus>
                         @error('email')
                             <span class="invalid-feedback" role="alert">
