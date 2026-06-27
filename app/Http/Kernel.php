25daf138ecde8a44bc1a055c0b6a00bc17645b0d
himanshusaini9024 <sainihimanshu208@gmail.com>
2026-06-27 10:20:53 +0530

    changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,7 @@ class Kernel extends HttpKernel
         'api' => [
                 \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
 
-            'throttle:60,1',
+            'throttle:10000,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
